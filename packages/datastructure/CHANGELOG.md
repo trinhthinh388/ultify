f4c98c79c1690fabb7394d01ba5e28b2c81f6c4d
Thinh Trinh Duc <thinh.duc.trinh@gmail.com>
2024-06-13 14:39:53 +0700

    :sparkles: (datastructure) added queue (#7) (#8)

--- packages/datastructure/CHANGELOG.md
+++ packages/datastructure/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @ultify/datastructure
 
+## 0.2.0
+
+### Minor Changes
+
+- Added Queue
+
 ## 0.1.0
 
 ### Minor Changes
---
+++ packages/datastructure/src/queue/queue.ts
@@ -0,0 +1,65 @@
+/**
+ * @license MIT
+ * @copyright 2024 Thinh Trinh Duc <thinh.duc.trinh@gmail.com>
+ *
+ * @class
+ */
+export class Queue<T> {
+  #array: T[] = [];
+
+  /**
+   * Create a new queue
+   */
+  constructor(...elements: T[]) {
+    this.#array = [...elements];
+  }
+
+  /**
+   * Creates a queue from the existing array
+   * @public
+   * @static
+   * @param {Array} [elements]
+   * @return {Stack}
+   */
+  static fromArray<T>(arr: T[]) {
+    return new Queue<T>(...arr);
+  }
+
+  /**
+   * Queue's size
+   * @readonly
+   */
+  get size() {
+    return this.#array.length;
+  }
+
+  /**
+   * Check if the queue is empty.
+   */
+  isEmpty() {
+    return this.#array.length === 0;
+  }
+
+  /**
+   * Enqueue new elements, and returns the new size of the queue.
+   */
+  enqueue(...value: T[]) {
+    return this.#array.push(...value);
+  }
+
+  /**
+   *
+   * Removes the frist element from a queue and returns it.
+   * If the queue is empty, undefined is returned and the queue is not modified.
+   */
+  dequeue() {
+    return this.#array.shift();
+  }
+
+  /**
+   * Clear queue
+   */
+  clear() {
+    this.#array = [];
+  }
+}
