a385b43e5156dea1ded1ee7b0d83bc883d7e6e59
Thinh Trinh Duc <thinh.duc.trinh@gmail.com>
2024-06-13 21:16:06 +0700

    :pencil: (homepage) update homepage

--- docs/src/components/home-page.tsx
+++ docs/src/components/home-page.tsx
@@ -26,8 +26,16 @@ export const HomePage = () => {
         <h1 className="font-display font-black text-[min(4.375rem,max(8vw,2.5rem))] tracking-wide mt-[3.4rem]">
           Ultify
         </h1>
-        <p className="text-[min(1.3rem,max(3.5vw,1.2rem))] mt-[1.5em]">
-          A modern typescript & javascript utility library.
+        <p className="text-[min(1.3rem,max(3.5vw,1.2rem))] mt-[1.3em]">
+          A modern{' '}
+          <span className="bg-[#3178c6] text-white font-bold px-2 py-1">
+            Typescript
+          </span>{' '}
+          &{' '}
+          <span className="bg-[#f6de37] font-bold px-2 py-1 text-black">
+            Javascript
+          </span>{' '}
+          utility library.
         </p>
       </div>
 
