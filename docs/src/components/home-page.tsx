c392edaf4b926d22ec5a1febe9694403a1f4e84a
Thinh Trinh Duc <thinh.duc.trinh@gmail.com>
2024-06-13 20:53:38 +0700

    :pencil: (docs) homepage

--- docs/src/components/home-page.tsx
+++ docs/src/components/home-page.tsx
@@ -1,7 +1,16 @@
+import { MaxWidthWrapper } from './max-width-wrapper';
+
 export const HomePage = () => {
   return (
-    <div>
-      <h1 className="font-display font-black text-5xl">Ultify</h1>
-    </div>
+    <MaxWidthWrapper>
+      <div className="mb-12">
+        <h1 className="font-display font-black text-[min(4.375rem,max(8vw,2.5rem))] tracking-wide mt-[3.4rem]">
+          Ultify
+        </h1>
+        <p className="text-[min(1.3rem,max(3.5vw,1.2rem))] mt-[1.5em]">
+          A modern typescript & javascript utility library.
+        </p>
+      </div>
+    </MaxWidthWrapper>
   );
 };
