9dbb7a35ed0c5fa18c8adda8a4099fd73f2aa155
Thinh Trinh Duc <thinh.duc.trinh@gmail.com>
2024-06-17 05:50:21 +0700

    :pencil: (docs) added heap docs

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,28 +1,29 @@
-import nextra from 'nextra'
-import remarkMdxDisableExplicitJsx from 'remark-mdx-disable-explicit-jsx'
+import nextra from 'nextra';
+import remarkMdxDisableExplicitJsx from 'remark-mdx-disable-explicit-jsx';
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   latex: true,
   flexsearch: {
-    codeblocks: false
+    codeblocks: false,
   },
   defaultShowCopyCode: true,
   mdxOptions: {
     remarkPlugins: [
       [
         remarkMdxDisableExplicitJsx,
-        { whiteList: ['table', 'thead', 'tbody', 'tr', 'th', 'td'] }
-      ]
-    ]
-  }
-})
+        { whiteList: ['table', 'thead', 'tbody', 'tr', 'th', 'td'] },
+      ],
+    ],
+  },
+});
 
 export default withNextra({
+  katex: true,
   reactStrictMode: true,
   eslint: {
     // ESLint behaves weirdly in this monorepo.
-    ignoreDuringBuilds: true
-  }
-})
+    ignoreDuringBuilds: true,
+  },
+});
--- docs/src/components/icons/index.ts
+++ docs/src/components/icons/index.ts
@@ -1,3 +1,4 @@
-export * from './function'
-export * from './queue'
-export * from './stack'
+export * from './function';
+export * from './queue';
+export * from './stack';
+export * from './grid-view';
