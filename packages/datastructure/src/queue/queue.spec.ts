4cc1c592d51cf25650ecda87f068f95fceb35efa
Thinh Trinh Duc <thinh.duc.trinh@gmail.com>
2024-06-19 15:06:00 +0700

    :recycle: (datastructure) updated Queue to use Linked List (#31)

--- packages/datastructure/src/queue/queue.spec.ts
+++ packages/datastructure/src/queue/queue.spec.ts
@@ -1,34 +1,39 @@
-import { Queue } from "./queue";
+import { Queue } from './queue';
 
 describe('Queue', () => {
   describe('Constructor', () => {
     it('Should be able to create new Queue with values', () => {
-      expect((new Queue(1,2,3)).toArray()).toEqual([1,2,3])
-    })
+      expect(new Queue(1, 2, 3).toArray()).toEqual([1, 2, 3]);
+    });
 
     it('Should be able to create a queue from an array', () => {
-      expect((Queue.fromArray([1,2,3])).toArray()).toEqual([1,2,3])
-    })
-  })
+      expect(Queue.fromArray([1, 2, 3]).toArray()).toEqual([1, 2, 3]);
+    });
+  });
 
   describe('Operations', () => {
-    const queue = new Queue()
     it('Should work correctly', () => {
-      expect(queue.isEmpty()).toBe(true)
-      queue.enqueue(1)
-      expect(queue.size).toBe(1)
-      expect(queue.isEmpty()).toBe(false)
-      expect(queue.peek()).toBe(1)
-      queue.dequeue()
-      expect(queue.size).toBe(0)
-      expect(queue.isEmpty()).toBe(true)
-      expect(queue.peek()).toBe(undefined)
+      const queue = new Queue();
+      expect(queue.isEmpty()).toBe(true);
+      queue.enqueue(1);
+      expect(queue.size).toBe(1);
+      expect(queue.isEmpty()).toBe(false);
+      expect(queue.peek()).toBe(1);
+      queue.dequeue();
+      expect(queue.size).toBe(0);
+      expect(queue.isEmpty()).toBe(true);
+      expect(queue.peek()).toBe(undefined);
+      expect(queue.dequeue()).toBe(undefined);
 
-      queue.enqueue(1, 2, 3)
-      queue.clear()
-      expect(queue.size).toBe(0)
-      expect(queue.isEmpty()).toBe(true)
-      expect(queue.peek()).toBe(undefined)
-    })
-  })
-})
+      queue.enqueue(1, 2, 3);
+
+      queue.dequeue();
+      queue.enqueue(3);
+
+      queue.clear();
+      expect(queue.size).toBe(0);
+      expect(queue.isEmpty()).toBe(true);
+      expect(queue.peek()).toBe(undefined);
+    });
+  });
+});
