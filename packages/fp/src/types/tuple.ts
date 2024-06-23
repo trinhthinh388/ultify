38ec16c74a1de94a154373c5eb51d6c2b92147b2
Thinh Trinh Duc <thinh.duc.trinh@gmail.com>
2024-06-23 12:20:12 +0700

    Fp/ariaty (#33)
    * :sparkles: (fp) added curry
    
    * :sparkles: (fp) working on curry
    
    * :sparkles: (fp) working on curry
    
    * :bookmark: (fp) release curry
    
    * :green_heart: (fp) fix build step
    
    * :construction_worker: (ci) added build test

---
+++ packages/fp/src/types/tuple.ts
@@ -0,0 +1,9 @@
+/**
+ * Creates a tuple of specified length.
+ *
+ * {@link https://itnext.io/implementing-arithmetic-within-typescripts-type-system-a1ef140a6f6f Gracefully borrowed from here}
+ */
+export type Tuple<
+  L extends number,
+  T extends any[] | readonly any[] = [],
+> = T extends { length: L } ? T : Tuple<L, [...T, any]>;
